b628a0d82d8ada94707ae29c7b3393ca94e6abc6
Beatriz Negreiros (IWS) <beatriznegreiros@outlook.com>
2020-05-25 14:16:27 +0200

    further developments in classes, d90 updated

--- PycharmProjects/FKAPHMM/codes/main/testing_classification.py
+++ PycharmProjects/FKAPHMM/codes/main/testing_classification.py
@@ -7,9 +7,10 @@ import earthpy as et
 import earthpy.plot as ep
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap, BoundaryNorm
+from matplotlib import cm
 
 # INPUT: d84 as parameter
-d = 0.1
+d = 0.013  # in cm
 
 current_dir = Path.cwd().parent.parent
 map_A_path = str(current_dir / "rasters/map_A.tif")
@@ -30,38 +31,40 @@ plt.savefig(fig, dpi=600)
 plt.clf()
 
 # Bins to classify the data
-# Legend: LD (Low Deposition), LID (Low Intermediate Deposition), ID (Intermediate Deposition)
-#         HID (High Intermediate Deposition), HD (High Deposition)
-#            [      static    LD     LID    ID     HID      HD
-class_bins = [raster_A.min(), 0.5 * d, 2 * d, 5 * d, 10 * d, 20 * d, 100 * d]
-# another option: class_bins = [-2*d, 2*d, 5*d, 10*d, 20*d, 100*d, 200*d]
+# Legend: HE (High Erosion), HIE (High Intermediate Erosion), IE (Intermediate Erosion), LIE (Low Intermediate Erosion),
+#         LE (Low Erosion), Static,  LD (Low Deposition), LID (Low Intermediate Deposition), ID (Intermediate Deposition),
+#         HID (High Intermediate Deposition), HD (High Deposition).
+class_bins = [-100*d, -20 * d, -10 * d, -5 * d, -2 * d, -0.5 * d, 0.5 * d, 2 * d, 5 * d, 10 * d, 20 * d, np.iinfo(np.int32).max]
 
 # Classify the original image array, then unravel it again for plotting
 raster_A_class = np.digitize(raster_A, class_bins)
-print(np.unique(raster_A_class))
-ep.hist(raster_A_class.ravel())
+
+raster_A_ma = np.ma.masked_where(raster_A_class == 0.0,
+                                 raster_A_class,
+                                 copy=True)
+
+print(np.unique(raster_A_ma))
+ep.hist(raster_A_ma.ravel())
 fig = str(current_dir / "analysis/hist_rasterA_classified.png")
 plt.savefig(fig, dpi=600)
 plt.clf()
 
-raster_A_class_ma = np.ma.masked_where(raster_A_class == 0,
-                                       raster_A_class,
-                                       copy=True)
-
 # Create a colormap from a list of colors
-colors = ['linen', 'lightgreen', 'darkgreen', 'maroon', 'darkblue']
+colors = ['darkred', 'red', 'orangered', 'tomato', 'lightsalmon', 'linen', 'lightskyblue', 'deepskyblue', 'dodgerblue',
+          'blue', 'darkblue']
+
 cmap = ListedColormap(colors)
 norm = BoundaryNorm(class_bins, len(colors))
-ep.plot_bands(raster_A_class_ma,
+ep.plot_bands(raster_A_ma,
               cmap=cmap,
               title="Classified Raster from Experimental Model (Map A)",
               scale=False,
               norm=norm)
 plt.show()
 
-raster_A_class_ma_fi = np.ma.filled(raster_A_class_ma, fill_value=nodatavalue)
-print(raster_A_class_ma_fi.min(), nodatavalue)
+raster_A_ma_fi = np.ma.filled(raster_A_ma, fill_value=nodatavalue)
+print(raster_A_ma_fi.min(), nodatavalue)
 
 out_path = str(current_dir / "rasters/map_A_classified.tif")
 with rio.open(out_path, 'w', **meta) as outf:
-    outf.write(raster_A_class_ma_fi.astype(rio.float32), 1)
+    outf.write(raster_A_ma_fi.astype(rio.float32), 1)
