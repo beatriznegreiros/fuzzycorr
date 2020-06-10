fb22dd23a2e939c5ae746ada2e837fc0c6c145ca
Beatriz Negreiros (IWS) <beatriznegreiros@outlook.com>
2020-06-10 12:02:45 +0200

    organized code sections

--- FMAPHMM/codes/main/fuzzy_num.py
+++ FMAPHMM/codes/main/fuzzy_num.py
@@ -6,27 +6,27 @@ try:
 except:
     print('ExceptionERROR: Missing fundamental packages (required: pathlib, numpy, gdal, pysal).')
 
+# 1. Create directory if not existent
 current_dir = Path.cwd().parent.parent
 Path(current_dir / "rasters").mkdir(exist_ok=True)
 
-# Neighborhood definition
-n = 4
+# 2. Neighborhood definition
+n = 4  # 'radius' of neighborhood
 halving_distance = 2
 
-# INPUT: Raster input path
-
+# 3. INPUT: Rasters input path
 map_A_in = str(current_dir / "rasters/map_A.tif")
 map_B_in = str(current_dir / "rasters/map_B.tif")
 
-# ------------------------------------------------
+# 4. Read rasters as arrays
 A = mo.MapArray(mo.raster_to_np(map_A_in))
 B = mo.MapArray(mo.raster_to_np(map_B_in))
 
-# Two-way similarity, first A x B then B x A
+# 5. Two-way similarity, first A x B then B x A
 s_AB = np.zeros(np.shape(A.array), dtype=float)
 s_BA = np.zeros(np.shape(A.array), dtype=float)
 
-# Loop to calculate similarity A x B
+#  5.1  Loop to calculate similarity A x B
 for index, a in np.ndenumerate(A.array):
     memb, neighbours = B.neighbours(index[0], index[1], n=n, halving_dist=halving_distance)
     f_i = -np.inf
@@ -38,7 +38,7 @@ for index, a in np.ndenumerate(A.array):
             f_i = f
     s_AB[index] = f_i
 
-# Loop to calculate similarity A x B
+#  5.2  Loop to calculate similarity B x A
 for index, a in np.ndenumerate(B.array):
     memb, neighbours = A.neighbours(index[0], index[1], n=n, halving_dist=halving_distance)
     f_i = -np.inf
@@ -50,10 +50,14 @@ for index, a in np.ndenumerate(B.array):
             f_i = f
     s_BA[index] = f_i
 
-# Mask pixels where there's no similarity measure
+# 6.  Mask pixels where there's no similarity measure
 S_i = np.minimum(s_AB, s_BA)
 S_i_ma = np.ma.masked_where(S_i == -np.inf,
                             S_i,
                             copy=True)
 S = S_i_ma.mean()
+# 7.  Local similarity
+
+
+# 8. Overall similarity
 print("The overall average similarity is:", S)
