cd24ef8333169da857ee454ff449cb469f18ad92
Beatriz Negreiros (IWS) <beatriznegreiros@outlook.com>
2020-07-23 18:30:12 +0200

    generated hydro stochastical calib comparison

--- salzach_case/fuzzy_salzach.py
+++ salzach_case/fuzzy_salzach.py
@@ -6,13 +6,13 @@ import fuzzyevaluation as fuzz
 # Neighborhood definition
 n = 8  # 'radius' of neighborhood
 halving_distance = 4
-comparison_name = "vali_hydroman_meas_simil_fuzzyRMSE_n8hd4"
+comparison_name = "vali_hydrostoch_meas_simil_n8hd4"
 
 # Create directory if not existent
 current_dir = Path.cwd()
 Path(current_dir / "rasters").mkdir(exist_ok=True)
 map_A_in = str(current_dir / "rasters/vali_meas_2013_clipped.tif")
-map_B_in = str(current_dir / "rasters/vali_hydro_FT_manual_2013_clipped.tif")
+map_B_in = str(current_dir / "rasters/vali_Hydro_FT-2D_MAP_2013_clipped.tif")
 # ------------------------------------------------------------------
 
 # Start run time count
@@ -20,7 +20,7 @@ start = timeit.default_timer()
 
 # Perform fuzzy comparison
 compareAB = fuzz.FuzzyComparison(map_A_in, map_B_in, current_dir, n, halving_distance)
-global_simil = compareAB.fuzzy_rmse(comparison_name)
+global_simil = compareAB.fuzzy_numerical(comparison_name)
 
 # Print global similarity
 print('Average fuzzy similarity:', global_simil)
