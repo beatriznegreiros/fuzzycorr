8f4a6fd54fd82ccae98821a89756f92e41485e51
Beatriz Negreiros (IWS) <beatriznegreiros@outlook.com>
2020-07-23 18:38:42 +0200

    delete un. files

--- test_plotter.py
+++
@@ -1,9 +0,0 @@
-import plotter
-from pathlib import Path
-
-current_dir = Path.cwd()
-rast_path = str(current_dir/'salzach_case') + '/results/' + 'vali_hydroman_meas_simil_fuzzynum.tif'
-rasterfuzzy = plotter.RasterDataPlotter(rast_path)
-
-path_fig = str(current_dir/'salzach_case') + '/results/' + 'vali_hydroman_meas_simil_fuzzynum.jpg'
-rasterfuzzy.plot_raster(path_fig)
