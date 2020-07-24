49b7795bda66f2987fbb33dfe48189bdd191216e
beatriznegreiros <beatriznegreiros@outlook.com>
2020-07-24 11:16:14 +0200

    reorg and deletion of deprecated note block

--- salzach_case/salzach_case.py
+++ salzach_case/salzach_case.py
@@ -6,15 +6,16 @@ import fuzzyevaluation as fuzz
 # ---------------Data Pre-processing---------------------------------
 # ------------------------INPUT--------------------------------------
 #  Raw data input path
-data_A = 'cali_Hydro_FT-2D_MAP_2010.csv'
-data_B = 'cali_meas_2010.csv'
+list_files = ['vali_aPC_MAP_2013',
+              'vali_hydro_FT_manual_2013',
+              'vali_Hydro_FT-2D_MAP_2013',
+              'vali_meas_2013']
+
+# Parameters
 attribute = 'dz'
+interpol_method = 'cubic'
 
-interpol_method = 'linear'
-name_map_A = 'cali_Hydro_FT-2D_MAP_2010_nearest_res1'
-name_map_B = 'cali_meas_2010_nearest_res1'
-
-# Polygon and raster of area of interest
+# Polygon of area of interest
 polyname = 'polygon_salzach'
 
 #  Raster Resolution: Change as appropriate
@@ -38,68 +39,22 @@ Path(current_dir / 'rasters').mkdir(exist_ok=True)
 
 poly_path = str(current_dir / 'shapefiles') + '/' + polyname + '.shp'
 
-list_files = ['cali_aPC_MAP_2010', 'cali_hydro_FT_manual_2010', 'cali_Hydro_FT-2D_MAP_2010',
-              'cali_meas_2010', 'vali_aPC_MAP_2013', 'vali_hydro_FT_manual_2013', 'vali_Hydro_FT-2D_MAP_2013',
-              'vali_meas_2013']
-
 for file in list_files:
+    # Path management
     path_file = str(current_dir / 'raw_data') + '/' + file + '.csv'
     name_file = file + '_res' + str(res)
     raster_file = str(current_dir / 'rasters') + '/' + file + '.tif'
+
+    # Instanciate object of SpatialField
     map_file = mo.SpatialField(name_file, pd.read_csv(path_file, skip_blank_lines=True), attribute=attribute, crs=crs, project_dir=current_dir, nodatavalue=nodatavalue, res=res, ulc=ulc, lrc=lrc)
-    clip_raster = str(current_dir / 'rasters') + '/' + file + '_clipped' + '.tif'
 
-    array_ = map_file.norm_array(method='cubic')
+    # Normalize points to a grid-ed array
+    array_ = map_file.norm_array(method=interpol_method)
+
+    # Write raster
     map_file.array2raster(array_, raster_file, save_ascii=True)
+
+    # Clip raster
+    clip_raster = str(current_dir / 'rasters') + '/' + file + '_clipped' + '.tif'
+    #  map_file.create_polygon(poly_path, alpha=0.01)
     map_file.clip_raster(poly_path, raster_file, clip_raster)
-
-'''if '.' not in data_A[-4:]:
-    data_A += '.csv'
-path_A = str(current_dir / 'raw_data') + '/' + data_A
-
-if '.' not in data_B[-4]:
-    data_A += '.csv'
-path_B = str(current_dir / 'raw_data') + '/' + data_B
-
-map_A = mo.SpatialField(name_map_A, pd.read_csv(path_A, skip_blank_lines=True), attribute=attribute, crs=crs,
-                        project_dir=current_dir, nodatavalue=nodatavalue)
-map_B = mo.SpatialField(name_map_B, pd.read_csv(path_B, skip_blank_lines=True), attribute=attribute, crs=crs,
-                        project_dir=current_dir, nodatavalue=nodatavalue)
-
-# Write rasters of interpolated values
-map_A.norm_raster(res, ulc=ulc, lrc=lrc, method='nearest')
-map_B.norm_raster(res, ulc=ulc, lrc=lrc, method='nearest')
-
-# Save shape of points cloud
-# map_A.shape()
-
-# Create polygon of interest area for comparison
-poly_path = str(current_dir / 'shapefiles') + '/' + polyname + '.shp'
-clip_raster_A = str(current_dir / 'rasters') + '/' + name_map_A + '_clipped' + '.tif'
-clip_raster_B = str(current_dir / 'rasters') + '/' + name_map_B + '_clipped' + '.tif'
-
-map_A.create_polygon(poly_path, alpha=0.01)
-map_A.clip_raster(poly_path, clip_raster_A)
-map_B.clip_raster(poly_path, clip_raster_B)'''
-
-# ---------------Fuzzy map comparison---------------------------------
-# ------------------------INPUT--------------------------------------
-# Neighborhood definition
-'''n = 4  # 'radius' of neighborhood
-halving_distance = 2
-comparison_name = "Hydro_FT_2010-2013_manual_simil"
-
-# Create directory if not existent
-dir = Path.cwd()
-Path(dir / "rasters").mkdir(exist_ok=True)
-map_A_in = str(dir / "rasters/dz_meas_2010-2013_norm.tif")
-map_B_in = str(dir / "rasters/Hydro_FT-2D_manual_2013_norm.tif")'''
-# ------------------------------------------------------------------
-
-
-# Perform fuzzy comparison
-'''compareAB = fuzz.FuzzyComparison(map_A_in, map_B_in, dir, n, halving_distance)
-global_simil = compareAB.fuzzy_numerical(comparison_name)
-
-# Print global similarity
-print('Average fuzzy similarity:', global_simil)'''
