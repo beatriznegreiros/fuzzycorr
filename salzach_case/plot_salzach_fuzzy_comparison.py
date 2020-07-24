d7d0bd8f90648f5993001e26dcbf526ffb1bd858
beatriznegreiros <beatriznegreiros@outlook.com>
2020-07-24 12:44:34 +0200

    new files

--- salzach_case/plot_salzach_fuzzy_comparison.py
+++ salzach_case/plot_salzach_fuzzy_comparison.py
@@ -2,8 +2,8 @@ import plotter
 from pathlib import Path
 
 current_dir = Path.cwd()
-rast_path = str(current_dir) + '/results/' + 'vali_hydrostoch_meas_simil_n8hd4.tif'
+rast_path = str(current_dir) + '/rasters/' + 'vali_aPC_MAP_2013_clipped.tif'
 rasterfuzzy = plotter.RasterDataPlotter(rast_path)
 
-path_fig = str(current_dir) + '/results/' + 'vali_hydrostoch_meas_simil_n8hd4.jpg'
+path_fig = str(current_dir) + '/rasters/' + 'vali_aPC_MAP_2013_clipped.jpg'
 rasterfuzzy.plot_raster(path_fig)
